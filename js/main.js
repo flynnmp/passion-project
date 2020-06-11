aae59a3655eefea825f6e7c0a86355c84ad07a66
flynnmp <62521077+flynnmp@users.noreply.github.com>
2020-06-11 22:52:41 +1200

    java update 1

---
+++ js/main.js
@@ -0,0 +1,130 @@
+console.log("HELLO WORLD!");
+
+
+function toggleMenu() {
+
+    //SET X TO BE OUR #menu ELEMENT
+    var x = document.getElementById("dropmenu");
+    //SET Y TO BE OUR #navButton ELEMENT
+    var y = document.getElementById("menuarrow");
+
+    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
+    x.classList.toggle("displayMenu");
+    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
+    y.classList.toggle("fa-chevron-up");
+
+}
+
+
+
+function expandContent1() {
+
+    //SET X TO BE OUR #menu ELEMENT
+    var x = document.getElementById("popup1");
+    //SET Y TO BE OUR #navButton ELEMENT
+    var y = document.getElementById("feature1");
+
+    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
+    x.classList.toggle("expandContent1");
+    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
+    
+
+}
+
+function expandContent2() {
+
+    //SET X TO BE OUR #menu ELEMENT
+    var x = document.getElementById("content2");
+    //SET Y TO BE OUR #navButton ELEMENT
+    var y = document.getElementById("menuplus2");
+
+    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
+    x.classList.toggle("expandContent2");
+    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
+    y.classList.toggle("fa-minus");
+
+}
+
+function expandContent3() {
+
+    //SET X TO BE OUR #menu ELEMENT
+    var x = document.getElementById("content3");
+    //SET Y TO BE OUR #navButton ELEMENT
+    var y = document.getElementById("menuplus3");
+
+    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
+    x.classList.toggle("expandContent3");
+    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
+    y.classList.toggle("fa-minus");
+
+}
+
+function expandContent4() {
+
+    //SET X TO BE OUR #menu ELEMENT
+    var x = document.getElementById("content4");
+    //SET Y TO BE OUR #navButton ELEMENT
+    var y = document.getElementById("menuplus4");
+
+    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
+    x.classList.toggle("expandContent4");
+    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
+    y.classList.toggle("fa-minus");
+
+}
+
+function expandContent5() {
+
+    //SET X TO BE OUR #menu ELEMENT
+    var x = document.getElementById("content5");
+    //SET Y TO BE OUR #navButton ELEMENT
+    var y = document.getElementById("menuplus5");
+
+    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
+    x.classList.toggle("expandContent5");
+    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
+    y.classList.toggle("fa-minus");
+
+}
+
+function expandContent6() {
+
+    //SET X TO BE OUR #menu ELEMENT
+    var x = document.getElementById("content6");
+    //SET Y TO BE OUR #navButton ELEMENT
+    var y = document.getElementById("menuplus6");
+
+    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
+    x.classList.toggle("expandContent6");
+    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
+    y.classList.toggle("fa-minus");
+
+}
+
+function expandContent7() {
+
+    //SET X TO BE OUR #menu ELEMENT
+    var x = document.getElementById("content7");
+    //SET Y TO BE OUR #navButton ELEMENT
+    var y = document.getElementById("menuplus7");
+
+    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
+    x.classList.toggle("expandContent7");
+    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
+    y.classList.toggle("fa-minus");
+
+}
+
+function expandContent8() {
+
+    //SET X TO BE OUR #menu ELEMENT
+    var x = document.getElementById("content8");
+    //SET Y TO BE OUR #navButton ELEMENT
+    var y = document.getElementById("menuplus8");
+
+    //TOGGLE (EITHER ADD OR REMOVE) THE .displayMenu CLASS TO #menu ELEMENT
+    x.classList.toggle("expandContent8");
+    //TOGGLE THE .fa-toggle-on CLASS FROM FONTAWESOME to the #navButton ELEMENT
+    y.classList.toggle("fa-minus");
+
+}
